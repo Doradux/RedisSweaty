273d9064aab1e8c1ad8f99c7124e1e6d8e16c2b8
Marcos Dorado Muñoz <38770399+Doradux@users.noreply.github.com>
2025-01-14 01:28:01 +0100

    1.01
    Enunciados

--- ejercicio.py
+++ ejercicio.py
@@ -372,11 +372,33 @@ funciones = {
     15: ej15, 16: ej16, 17: ej17, 18: ej18,
 }
 
+enunciados = {
+    1: " Crear registros clave-valor(0.5 puntos)",
+    2: " Obtener y mostrar el número de claves registradas (0.5 puntos)",
+    3: " Obtener y mostrar un registro en base a una clave (0.5 puntos)",
+    4: " Actualizar el valor de una clave y mostrar el nuevo valor(0.5 puntos)",
+    5: " Eliminar una clave-valor y mostrar la clave y el valor eliminado(0.5 puntos)",
+    6: " Obtener y mostrar todas las claves guardadas (0.5 puntos)",
+    7: " Obtener y mostrar todos los valores guardados(0.5 puntos)",
+    8: " Obtener y mostrar varios registros con una clave con un patrón en común usando * (0.5 puntos)",
+    9: " Obtener y mostrar varios registros con una clave con un patrón en común usando [] (0.5 puntos)",
+    10: " Obtener y mostrar varios registros con una clave con un patrón en común usando ? (0.5 puntos)",
+    11: " Obtener y mostrar varios registros y filtrarlos por un valor en concreto. (0.5 puntos)",
+    12: " Actualizar una serie de registros en base a un filtro (por ejemplo aumentar su valor en 1 )(0.5 puntos)",
+    13: " Eliminar una serie de registros en base a un filtro (0.5 puntos)",
+    14: " Crear una estructura en JSON de array de los datos que vayais a almacenar(0.5 puntos)",
+    15: " Realizar un filtro por cada atributo de la estructura JSON anterior (0.5 puntos)",
+    16: " Crear una lista en Redis (0.5 puntos)",
+    17: " Obtener elementos de una lista con un filtro en concreto(0.5 puntos)",
+    18: " En Redis hay otras formas de almacenar datos: Set, Hashes, SortedSet,Streams, Geopatial, Bitmaps, Bitfields,Probabilistic y Time Series. Elige dos de estos tipos, y crea una función que los guarde en la base de datos y otra que los obtenga. (1.5 puntos)"
+}
+
 def mostrar_menu():
     os.system('cls' if os.name == 'nt' else 'clear')
     print("\n--- Menu ---")
+    
     for i in range(1, 19):
-        print(f"{i}. Execute ej{i}")
+        print(f"{i}. {enunciados[i]}")
     print("19. Exit")
     
 def pulsa_para_continuar():
@@ -384,21 +406,22 @@ def pulsa_para_continuar():
     os.system('cls' if os.name == 'nt' else 'clear')
 
 pulsa_para_continuar()
+
 while True:
-        mostrar_menu()
-        try:
-            opcion = int(input("Select an option: "))
+    mostrar_menu()
+    try:
+        opcion = int(input("Select an option: "))
+        print(opcion)
+        if opcion == 19:
+            print("diabolico")
+            break
+        elif opcion in funciones:
+            os.system('cls' if os.name == 'nt' else 'clear')
             print(opcion)
-            if opcion == 19:
-                print("diabolico")
-                break
-            elif opcion in funciones:
-                os.system('cls' if os.name == 'nt' else 'clear')
-                print(opcion)
-                funciones[opcion]()
-            else:
-                print("Not a valid option")
-        except ValueError:
-            print("Insert a valid number")
-            
-        pulsa_para_continuar()
+            funciones[opcion]()
+        else:
+            print("Not a valid option")
+    except ValueError:
+        print("Insert a valid number")
+        
+    pulsa_para_continuar()
